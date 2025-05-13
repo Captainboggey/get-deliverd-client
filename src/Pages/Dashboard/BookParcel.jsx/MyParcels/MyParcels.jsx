0a12115cadc55a576511b5d3ed2a95219ba60509
Captainboggey <anik35-700@diu.edu.bd>
2025-05-13 15:18:57 +0600

    update parcel added

--- src/Pages/Dashboard/BookParcel.jsx/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/BookParcel.jsx/MyParcels/MyParcels.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
 import useAuth from '../../../../Hooks/useAuth';
+import { useForm } from "react-hook-form";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
     const { user } = useAuth()
     const axiosPublic = useAxiosPublic()
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const { data: parcels = [] } = useQuery({
         queryKey: ['parcels', user?.email],
         queryFn: async () => {
@@ -14,6 +19,40 @@ const MyParcels = () => {
             return result.data
         }
     })
+    const onSubmit = (data) => {
+        const updateInfo = {
+            senderPhone: data.senderPhone,
+            parcelType: data.parcelType,
+            receiverName: data.receiverName,
+            receiverPhone: data.receiverPhone,
+            deliveryDate: data.deliveryDate,
+            deliveryLatitude: data.deliveryLatitude,
+            deliveryLongitude: data.deliveryLongitude
+        }
+
+        axiosPublic.patch(`/parcels/${data.id}`, updateInfo)
+            .then(res => {
+                if (res.data.modifiedCount > 0) {
+                    const Toast = Swal.mixin({
+
+                        toast: true,
+                        position: "top-end",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.onmouseenter = Swal.stopTimer;
+                            toast.onmouseleave = Swal.resumeTimer;
+                        }
+                    });
+                    Toast.fire({
+                        icon: "success",
+                        title: "Updated successfully"
+                    });
+                }
+            })
+    }
+
     return (
         <div>
             <Helmet>
@@ -24,21 +63,89 @@ const MyParcels = () => {
                 {
                     parcels.map(parcel => <div className='mx-auto' key={parcel._id}>
                         <div key={parcel._id} className="card bg-base-100 w-96 shadow-xl">
-                        
-                        <div className="card-body">
-                            <div className='flex justify-between'>
-                                <h2 className="card-title">Parcel Type {parcel.parcelType}</h2>
-                                <h2 className="card-title"><button className='btn btn-sm btn-primary'>Update</button><button className='btn btn-sm btn-secondary'>Cancel</button></h2>
-                            </div>
-                            <p>Requested Delivery Date {parcel.deliveryDate}</p>
-                            <p>Booking Date {parcel.bookingDate}</p>
-                            <p>Delivery Men ID </p>
-                            <p>Booking Status <span><button  className='btn btn-sm bg-yellow-700 text-white'>{parcel.status}</button></span> <span><button   className='btn btn-sm bg-yellow-500 text-white'>Review</button></span> </p>
-                            <div className="card-actions justify-start">
-                                <button className="btn btn-primary bg-green-600 text-white border-none">Pay</button>
+
+                            <div className="card-body">
+                                <div className='flex justify-between'>
+                                    <h2 className="card-title">Parcel Type {parcel.parcelType}</h2>
+                                    <h2 className="card-title"> <Popup trigger={<button className="btn btn-sm btn-secondary"> Update </button>} modal >
+
+                                        <span>
+
+                                            <div className="card bg-base-100 md::w-full  md:max-w-3xl mx-auto shrink-0 shadow-2xl">
+                                                <p className='text-xl text-center font-bold my-5'>Update Your Parcel</p>
+                                                <form onSubmit={handleSubmit(onSubmit)} className="card-body grid grid-cols-1 md:grid-cols-2 gap-2 justify-center mx-auto">
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Phone Number</span>
+                                                        </label>
+                                                        <input {...register("senderPhone")} type="text" placeholder="Phone" name='senderPhone' className="input  input-bordered" required />
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Parcel Type</span>
+                                                        </label>
+                                                        <input {...register("parcelType")} name='parcelType' type="text" placeholder="parcel type" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Receiver's Name</span>
+                                                        </label>
+                                                        <input {...register("receiverName")} type="text" placeholder="receiver name" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Receiver's Phone Number</span>
+                                                        </label>
+                                                        <input {...register("receiverPhone")} type="text" placeholder="receiver phone number" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Requested Delivery Date</span>
+                                                        </label>
+                                                        <input {...register("deliveryDate")} type="date" placeholder="Requested Delivery Dater" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Delivery Address Latitude</span>
+                                                        </label>
+                                                        <input {...register("deliveryLatitude")} type="text" placeholder="Latitude" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Delivery Address Longitude</span>
+                                                        </label>
+                                                        <input {...register("deliveryLongitude")} type="text" placeholder="Longitude" className="input input-bordered" required />
+
+                                                    </div>
+                                                    <div className="form-control mr-2 md:mx-0">
+                                                        <label className="label w-full">
+                                                            <span className="label-text">Parcel Id</span>
+                                                        </label>
+                                                        <input {...register("id")} disabled type="text" value={parcel._id} className="input input-bordered" required />
+
+                                                    </div>
+
+                                                    <div className="form-control col-span-2 mt-6 mr-2 md:mx-0">
+                                                        <button className="btn border-none btn-primary bg-green-700 w-full text-white">Update</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </span>  </Popup><button className='btn btn-sm btn-secondary'>Cancel</button></h2>
+                                </div>
+                                <p>Requested Delivery Date {parcel.deliveryDate}</p>
+                                <p>Booking Date {parcel.bookingDate}</p>
+                                <p>Delivery Men ID </p>
+                                <p>Booking Status <span><button className='btn btn-sm bg-yellow-700 text-white'>{parcel.status}</button></span> <span><button className='btn btn-sm bg-yellow-500 text-white'>Review</button></span> </p>
+                                <div className="card-actions justify-start">
+                                    <button className="btn btn-primary bg-green-600 text-white border-none">Pay</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>)
                 }
             </div>
